4401b398c70f6386e29d14c68ce550427d21f914
Juan Ortega <juan.ortega@theksquaregroup.com>
2024-05-18 21:44:18 -0600

    add sf utils class

--- IA-Engine/sf-utils.js
+++ IA-Engine/sf-utils.js
@@ -1,41 +1,79 @@
 const jsforce = require('jsforce');
-let conn;
 
-const login = async ({loginUrl, clientId, clientSecret, redirectUri}) => {
+const patientFieldsByType = new Map().
+    set('id', 'Id').
+    set('firstName', 'FirstName').
+    set('lastName', 'LastName').
+    set('email', 'Email').
+    set('mobilePhone', 'MobilePhone').
+    set('birthdate', 'Birthdate').
+    set('height', 'Height__c').
+    set('weight', 'Weight__c');
+
+const goalsFieldsByType = new Map().
+    set('patientId', 'PatientId__c').
+    set('name', 'Name').
+    set('description', 'Description__c').
+    set('targetDate', 'TargetDate__c');
+
+const login = async (properties) => {
+    const {
+        loginUrl, 
+        clientId, 
+        clientSecret, 
+        redirectUri, 
+        username, 
+        password, 
+        securityToken
+    } = properties;
+    
+    
     const conn = await new jsforce.Connection({
         oauth2 : {
             loginUrl : loginUrl,
             clientId : clientId,
             clientSecret : clientSecret,
             redirectUri : redirectUri
-        }
-    });
+    }});
+
+    const userInfo = await conn.login(username, password+securityToken);
+    console.log(userInfo);
 
     return conn;
 }
 
-const setPatientProperties = async (conn, properties) => {
-    const rets = await conn.sobject('Contact').update([{
-        Id : properties.id,
-        FirstName : properties.firstName,
-        LastName : properties.lastName,
-        Email : properties.email,
-        Mobile : properties.mobile,
-        Birthdate : properties.birth,
-        Age__c : properties.age,
-        Height__c : properties.height,
-        Weight__c : properties.weight,
-        Thread__c : properties.thread
-    }]);
-
-    return rets[0];
-}
+const setPatientProperties = async (conn, patient) => {
+    const properties = getProperties(patientFieldsByType, patient);
+    const ret = await conn.sobject('Contact').update(properties);
+    return ret;
+};
 
 const getPatientProperties = async (conn, id) => {
     const contact = await conn.sobject('Contact').retrieve(id);
     return new Patient(contact);
-}
+};
 
+const createPatientGoals = async (conn, goals) => {
+    const formattedGoals = goals.map(goal => getProperties(goalsFieldsByType, goal));
+    console.log(JSON.stringify(formattedGoals, null, 2));
+    
+    const rets = await conn.sobject('Goal__c').create(formattedGoals[0]);
+    return rets;
+
+};
+
+const getProperties = (map, obj) => {
+    const properties = {};
+
+    for (const [key, value] of Object.entries(obj)) {
+        const field = map.get(key);
+        if (field) {
+            properties[field] = value;
+        }
+    }
+
+    return properties;
+}
 class Patient {
     constructor(contact) {
         this.id = contact.Id;
@@ -54,5 +92,6 @@ class Patient {
 module.exports = {
     login, 
     setPatientProperties,
-    getPatientProperties
+    getPatientProperties,
+    createPatientGoals
 };
