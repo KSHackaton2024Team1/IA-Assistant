6551a823f400513a79a4939bff7ba0ad5383fb95
Alejandro-KS <64914441+Alejandro-KS@users.noreply.github.com>
2024-05-19 11:28:37 -0600

    add functions to IA

---
+++ IA-Engine/eventHandler.mjs
@@ -0,0 +1,73 @@
+import { setPatientProperties, getPatientProperties } from './sf-utils.mjs';
+
+export class EventHandler extends EventEmitter {
+	constructor(client, conn) {
+		super();
+		this.client = client;
+		this.conn = conn;
+	}
+
+	async onEvent(event) {
+		try {
+			// Retrieve events that are denoted with 'requires_action'
+			// since these will have our tool_calls
+			console.log("Event:", event.event);
+			if (event.event === "thread.run.requires_action") {
+				await this.handleRequiresAction(
+					event.data,
+					event.data.id,
+					event.data.thread_id,
+				);
+			}
+		} catch (error) {
+			console.error("Error handling event:", error);
+		}
+	}
+
+	async handleRequiresAction(data, runId, threadId) {
+		try {
+			const toolOutputs =
+			data.required_action.submit_tool_outputs.tool_calls.map(async (toolCall) => {
+				if (toolCall.function.name === "setPatientProperties") {
+					console.log("setPatientProperties: " + toolCall.function.arguments);
+					let { id, weight, height, mobile, age } = JSON.parse(toolCall.function.arguments);
+
+					let response = await setPatientProperties(this.conn, { id, weight, height, mobile, age });
+					return {
+						tool_call_id: toolCall.id,
+						output: response,
+					};
+				} else if (toolCall.function.name === "getPatientInformation") {
+					console.log("getPatientInformation");
+					let { id } = JSON.parse(toolCall.function.arguments);
+
+					let response = await getPatientProperties(this.conn, id);
+					return {
+						tool_call_id: toolCall.id,
+						output: response,
+					};
+				}
+			});
+			// Submit all the tool outputs at the same time
+			await this.submitToolOutputs(toolOutputs, runId, threadId);
+		} catch (error) {
+			console.error("Error processing required action:", error);
+		}
+	}
+
+	async submitToolOutputs(toolOutputs, runId, threadId) {
+		try {
+			// Use the submitToolOutputsStream helper
+			const stream = this.client.beta.threads.runs.submitToolOutputsStream(
+				threadId,
+				runId,
+				{ tool_outputs: toolOutputs },
+			);
+			for await (const event of stream) {
+				this.emit("event", event);
+			}
+		} catch (error) {
+			console.error("Error submitting tool outputs:", error);
+		}
+	}
+}
