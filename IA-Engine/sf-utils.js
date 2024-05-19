46bc770ae1088bb47ad73ae81f6fcaba42b0799c
Juan Ortega <juan.ortega@theksquaregroup.com>
2024-05-18 18:24:27 -0600

    add get patient properties

--- IA-Engine/sf-utils.js
+++ IA-Engine/sf-utils.js
@@ -1,11 +1,10 @@
 const jsforce = require('jsforce');
 let conn;
 
-const login = async ({clientId, clientSecret, redirectUri}) => {
+const login = async ({loginUrl, clientId, clientSecret, redirectUri}) => {
     const conn = await new jsforce.Connection({
         oauth2 : {
-          // you can change loginUrl to connect to sandbox or prerelease env.
-          // loginUrl : 'https://test.salesforce.com',
+            loginUrl : loginUrl,
             clientId : clientId,
             clientSecret : clientSecret,
             redirectUri : redirectUri
@@ -32,7 +31,28 @@ const setPatientProperties = async (conn, properties) => {
     return rets[0];
 }
 
+const getPatientProperties = async (conn, id) => {
+    const contact = await conn.sobject('Contact').retrieve(id);
+    return new Patient(contact);
+}
+
+class Patient {
+    constructor(contact) {
+        this.id = contact.Id;
+        this.firstName = contact.FirstName;
+        this.lastName = contact.LastName;
+        this.email = contact.Email;
+        this.mobile = contact.Mobile;
+        this.birth = contact.Birthdate;
+        this.age = contact.Age__c;
+        this.height = contact.Height__c;
+        this.weight = contact.Weight__c;
+        this.thread = contact.Thread__c;
+    }
+}
+
 module.exports = {
     login, 
     setPatientProperties,
+    getPatientProperties
 };
